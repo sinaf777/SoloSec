70026b4c10e485ca4bab4f784dd51b13ffcac1d7
sinaf777 <sinafdebelireal@gmail.com>
2025-08-29 17:43:41 +0300

    second commit 1-2 Mvp done

---
+++ SoloSecV1/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    score = models.IntegerField(default=0)
+    
+    
+    def __str__(self):
+        return self.username
---
+++ SoloSecV1/users/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = UserCreationForm()
+    return render (request, "users/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user=form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render (request, "users/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def profile_view(request):
+    return render(request, "users/profile.html")
+
+def home_view(request):
+    return render(request, "users/home.html")
