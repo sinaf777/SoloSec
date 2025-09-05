980b690373261cbdd546c77e449a44870eb16a56
Sinaf <166249512+sinaf777@users.noreply.github.com>
2025-09-05 09:22:05 +0300

    Delete SoloSecV1 directory

--- SoloSecV1/challenges/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.challenge_list, name="challenge_list"),
-    path("<int:challenge_id>/", views.challenge_detail, name="challenge_detail"),
-]
